b6a129fbcee8c0c04a3ab49cd248b15ebde3cfe4
Candice Davidson <cndragn@gmail.com>
2018-12-22 21:27:05 -0800

    install bootstrap, modify navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,43 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class Navigation extends React.Component {
 	render() {
 		return (
 			<div>
-				<nav className="grey darken-4">
-					<div className="container nav-wrapper">
-						<a href="#!" className="brand-logo">
-							ClickItOn
-						</a>
-						<a href="http://#" data-target="mobile-demo" className="sidenav-trigger">
-							<i className="material-icons">menu</i>
-						</a>
-						<ul className="right hide-on-med-and-down">
-							<li>
-								<Link to="/">Home</Link>
-							</li>
-							<li>
-								<Link to="/featured-movies">In Theaters Now</Link>
-							</li>
-							<li>
-								<a href="three.html">Three</a>
-							</li>
-						</ul>
-					</div>
-				</nav>
-
-				<ul className="sidenav" id="mobile-demo">
-					<li>
-						<a href="one.html">One</a>
-					</li>
-					<li>
-						<a href="two.html">Two</a>
-					</li>
-					<li>
-						<a href="three.html">Three</a>
-					</li>
-				</ul>
+				<Navbar inverse collapseOnSelect>
+					<Navbar.Header>
+						<Navbar.Brand>
+							<a href="#brand">ClickItOn</a>
+						</Navbar.Brand>
+						<Navbar.Toggle />
+					</Navbar.Header>
+					<Navbar.Collapse>
+						<Nav pullRight>
+							<LinkContainer to="/">
+								<NavItem eventKey={1}>Home</NavItem>
+							</LinkContainer>
+							<LinkContainer to="/featured-movies">
+								<NavItem eventKey={2} href="#">
+									In Theaters Now
+								</NavItem>
+							</LinkContainer>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
 			</div>
 		);
 	}
