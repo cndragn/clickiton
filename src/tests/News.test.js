9977e9436b015593a110f57cd4408ba9fa58d35d
Candice Davidson <cndragn@gmail.com>
2018-12-24 01:08:53 -0800

    update News test

--- src/tests/News.test.js
+++ src/tests/News.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import News from '../components/frontpage/News';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
+	ReactDOM.render(<News />, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
