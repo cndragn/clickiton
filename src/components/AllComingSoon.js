9cdadcdd5626a40eb0a8f182b600b019729464cb
Candice Davidson <cndragn@gmail.com>
2019-01-05 22:59:01 -0800

    show movie title if no poster image

--- src/components/AllComingSoon.js
+++ src/components/AllComingSoon.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { fetchComingSoon, titleLink } from '../helpers/movies';
 import { Link } from 'react-router-dom';
 import { Col } from 'react-bootstrap';
-import noImg from '../images/no-image.png';
+import noImg from '../images/no-image2.png';
 
 class AllComingSoon extends Component {
 	state = {
@@ -13,14 +13,14 @@ class AllComingSoon extends Component {
 	componentDidMount() {
 		axios.get(fetchComingSoon()).then((res) => {
 			const movies = res.data.results;
-			movies.map((movie) => {
-				if (movie.poster_path === null) {
-					movie.poster_path = noImg;
-				} else {
-					movie.poster_path = `https://image.tmdb.org/t/p/w342${movie.poster_path}`;
-				}
-				return 0;
-			});
+			// movies.map((movie) => {
+			// 	if (movie.poster_path === null) {
+			// 		movie.poster_path = noImg;
+			// 	} else {
+			// 		movie.poster_path = `https://image.tmdb.org/t/p/w342${movie.poster_path}`;
+			// 	}
+			// 	return 0;
+			// });
 			this.setState({ movies });
 		});
 	}
@@ -35,9 +35,14 @@ class AllComingSoon extends Component {
 						<h2>Coming Soon</h2>
 						<div className="show-grid auto-clear">
 							{movies.map(({ id, title, poster_path }) => (
-								<Col xs={6} md={3} lg={2} key={id}>
+								<Col xs={6} md={3} lg={2} key={id} className="poster-wrap">
 									<Link to={`/movie/${id}/${titleLink(title)}`}>
-										<img alt={title} title={title} src={poster_path} />
+										<img
+											alt={title}
+											title={title}
+											src={poster_path ? `https://image.tmdb.org/t/p/w342${poster_path}` : noImg}
+										/>
+										<p className={poster_path ? 'hidden' : ''}>{title}</p>
 									</Link>
 								</Col>
 							))}
