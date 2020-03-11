dcc866bce1dbd6a7ed12768cbbe3d6995dad1d70
Candice <cndragn@gmail.com>
2020-03-11 01:13:11 -0700

    link news articles on front page

--- src/components/frontpage/NewReleases.js
+++ src/components/frontpage/NewReleases.js
@@ -1,70 +1,71 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchNewReleases } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchNewReleases } from "../../actions";
 
-import { tidyLink } from '../../helpers/movies';
-import { Col, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { LinkContainer } from 'react-router-bootstrap';
+import { tidyLink } from "../../helpers/movies";
+import { Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 class NewReleases extends Component {
-	componentDidMount() {
-		this.props.fetchNewReleases();
-	}
+  componentDidMount() {
+    this.props.fetchNewReleases();
+  }
 
-	movieList = (num) => {
-		const { movies } = this.props;
-		let movieArr = [];
-		Object(movies).forEach(function(movie, i) {
-			if (movie.poster_path !== null && movieArr.length < num) {
-				movieArr.push(movie);
-			}
-		});
-		return movieArr;
-	};
+  movieList = num => {
+    const { movies } = this.props;
+    let movieArr = [];
+    Object(movies).forEach(function(movie, i) {
+      if (movie.poster_path !== null && movieArr.length < num) {
+        movieArr.push(movie);
+      }
+    });
+    return movieArr;
+  };
 
-	render() {
-		const { backdrop } = this.props;
-		return (
-			<div
-				className="featured-bg"
-				style={{
-					backgroundImage: `url(${backdrop})`
-				}}
-			>
-				<div className="featured">
-					<div className="container">
-						<div className="featured-title">
-							<h2>New Releases</h2>
-							<LinkContainer to="/new-releases">
-								<Button>View All</Button>
-							</LinkContainer>
-						</div>
+  render() {
+    console.log(this.props);
+    const { backdrop } = this.props;
+    return (
+      <div
+        className="featured-bg"
+        style={{
+          backgroundImage: `url(${backdrop})`
+        }}
+      >
+        <div className="featured">
+          <div className="container">
+            <div className="featured-title">
+              <h2>New Releases</h2>
+              <LinkContainer to="/new-releases">
+                <Button>View All</Button>
+              </LinkContainer>
+            </div>
 
-						<div className="show-grid auto-clear" />
-						{this.movieList(6).map(({ id, title, poster_path }) => (
-							<Col xs={4} md={2} key={id}>
-								<Link to={`/movie/${id}/${tidyLink(title)}`}>
-									<img
-										alt={title}
-										title={title}
-										src={`https://image.tmdb.org/t/p/w342${poster_path}`}
-									/>
-								</Link>
-							</Col>
-						))}
-					</div>
-				</div>
-			</div>
-		);
-	}
+            <div className="show-grid auto-clear" />
+            {this.movieList(6).map(({ id, title, poster_path }) => (
+              <Col xs={4} md={2} key={id}>
+                <Link to={`/movie/${id}/${tidyLink(title)}`}>
+                  <img
+                    alt={title}
+                    title={title}
+                    src={`https://image.tmdb.org/t/p/w342${poster_path}`}
+                  />
+                </Link>
+              </Col>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-	return {
-		movies: state.newReleases,
-		backdrop: state.backdrop
-	};
+const mapStateToProps = state => {
+  return {
+    movies: state.newReleases,
+    backdrop: state.backdrop
+  };
 };
 
 export default connect(mapStateToProps, { fetchNewReleases })(NewReleases);
--- src/components/frontpage/News.js
+++ src/components/frontpage/News.js
@@ -1,59 +1,61 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchNews } from '../../actions';
-
-import { Col } from 'react-bootstrap';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchNews } from "../../actions";
+// import { Link } from "react-router-dom";
 
 class News extends React.Component {
-	componentDidMount() {
-		this.props.fetchNews();
-	}
+  componentDidMount() {
+    this.props.fetchNews();
+  }
 
-	newsLimit() {
-		const { articles } = this.props;
+  newsLimit() {
+    const { articles } = this.props;
+    let news = [];
+    Object(articles).forEach(function(article, i) {
+      if (i < 4) {
+        news.push(article);
+      }
+    });
+    return news;
+  }
 
-		let news = [];
-		Object(articles).forEach(function(article, i) {
-			if (i < 4) {
-				news.push(article);
-			}
-		});
-		return news;
-	}
+  shorten(str) {
+    var length = 90;
+    return str.length > length ? str.substring(0, length - 3) + "..." : str;
+  }
 
-	shorten(str) {
-		var length = 90;
-		return str.length > length ? str.substring(0, length - 3) + '...' : str;
-	}
-
-	render() {
-		return (
-			<div className="container">
-				<h2>News</h2>
-				<div className="show-grid auto-clear">
-					{this.newsLimit().map(({ id, title, urlToImage }) => (
-						<Col xs={12} md={6} key={title}>
-							<div className="media">
-								<div className="media-left">
-									<img className="media-object" alt={title} src={urlToImage} />
-								</div>
-								<div className="media-body">
-									<h4 className="media-heading">{title.replace(/<.*?>/gm, '')}</h4>
-									<p>
-										<br />READ ARTICLE
-									</p>
-								</div>
-							</div>
-						</Col>
-					))}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <h2>News</h2>
+        <div className="news-wrapper">
+          {this.newsLimit().map(
+            ({ title, description, source, url, urlToImage }, id) => (
+              <div className="card media">
+                <div className="media-left">
+                  <img className="media-object" alt={title} src={urlToImage} />
+                </div>
+                <div className="media-body">
+                  <a href={url} target="_blank" rel="noopener noreferrer">
+                    <h4 className="media-heading">
+                      {title.replace(/<.*?>/gm, "")}
+                    </h4>
+                  </a>
+                  <p>{source.name}</p>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-	return { articles: state.articles };
+const mapStateToProps = state => {
+  return { articles: state.articles };
 };
 
 export default connect(mapStateToProps, { fetchNews })(News);
+
+// title, description, source, url, publishedAt, content, urlToImage;
